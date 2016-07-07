14cdd875b8b0f8058599a206c080c099230cf26e
钟慰 <auronzhong@gmail.com>
2016-07-07 19:43:32 +0800

    Week6作业

--- frontend/entry.js
+++ frontend/entry.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import {Router, Route, Link, browserHistory} from 'react-router'
 import App from './App';
 import TopicDetail from './component/TopicDetail';
+import Login from './component/Login';
+
 
 const e = document.createElement('div');
 e.id = 'app';
@@ -13,6 +15,7 @@ ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
             <Route path="topic/:id" component={TopicDetail}/>
+            <Route path="login" component={Login}/>
         </Route>
     </Router>
 ), e);
