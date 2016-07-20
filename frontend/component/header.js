9bc9702b36f6658efc61dc9857d6483d28996be6
钟慰 <auronzhong@gmail.com>
2016-07-20 21:40:23 +0800

    lesson8

--- frontend/component/header.js
+++ frontend/component/header.js
@@ -26,28 +26,33 @@ export default class Header extends React.Component {
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
-                        <a className="navbar-brand" href="#">简单论坛系统</a>
+                        <Link className="navbar-brand" to="#">简单论坛系统</Link>
                     </div>
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav">
                             <li className="active">
                                 <Link to="/">首页</Link>
                             </li>
-                            <li><a href="/new"><i className="glyphicon glyphicon-plus"></i> 发帖</a></li>
+                            <li><Link to="/new"><i className="glyphicon glyphicon-plus"></i> 发帖</Link></li>
                         </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            {this.state.user ? (
-                                <li><a onClick={this.handleLogout.bind(this)}>注销 [{this.state.user.nickname}]</a></li>
-                            ) : (
-                                <li><a href="/login">登录</a></li>
-                            )}
-                        </ul>
+                        {this.state.user ? (
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><a href="#" onClick={this.handleLogout.bind(this)}>注销
+                                    [{this.state.user.nickname}]</a></li>
+                            </ul>
+                        ) : (
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><Link to="/login">登录</Link></li>
+                                <li><Link to="/signup">注册</Link></li>
+                            </ul>
+                        )}
                     </div>
                 </div>
             </nav>
