e59c55e22ce60f7a06f639d98cf52d250dae8378
钟慰 <auronzhong@gmail.com>
2016-08-05 22:10:39 +0800

    lesson10作业

---
+++ src/methods/mail.js
@@ -0,0 +1,36 @@
+/**
+ * Created by zhongwei on 16/8/5.
+ */
+'use strict';
+
+/**
+ * pratice Node.js project
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+import nodemailer from 'nodemailer';
+
+module.exports = function (done) {
+
+
+    $.smtp = nodemailer.createTransport($.config.get('smtp'), {
+        from: $.config.get('smtp.auth.user'),
+    });
+
+
+    $.method('mail.send').check({
+        to: {required: true},
+        subject: {required: true},
+        html: {required: true},
+    });
+    $.method('mail.send').register(function (params, callback) {
+
+        $.smtp.sendMail(params, callback);
+
+    });
+
+
+    done();
+
+};
